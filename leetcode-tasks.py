7c3514b7db30e70ad8a637da03bcdc9437b3078c
Yugoff <vv.yugoff@gmail.com>
2024-01-09 16:59:59 +0500

    Added a solution to task 13: Roman to Integer

--- leetcode-tasks.py
+++ leetcode-tasks.py
@@ -30,4 +30,30 @@ class Solution:
             if x_str[i] != x_str[-i-1]:
                 return False
         return True
+
+# 13. Roman to Integer
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_map = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+        }
+        result = 0
+        i = 0
+
+        while i < len(s):
+            if i < len(s) - 1 and roman_map[s[i]] < roman_map[s[i+1]]:
+                result += roman_map[s[i+1]] - roman_map[s[i]]
+                i += 2
+            else:
+                result += roman_map[s[i]]
+                i += 1
+
+        return result
         
