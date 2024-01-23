a322a92f5e8b4c6c728fc30fe5cf7535a22b8527
Yugoff <vv.yugoff@gmail.com>
2024-01-23 17:16:55 +0500

    Add Solution 35

--- leetcode-tasks.py
+++ leetcode-tasks.py
@@ -101,3 +101,23 @@ class Solution:
                 nums[k] = nums[i]
                 k += 1
         return k
+
+# 35. Search Insert Position
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+    
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if nums[mid] == target:
+                return mid
+            
+            if target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        return left
