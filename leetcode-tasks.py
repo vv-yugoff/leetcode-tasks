7e8b586f7facf5f16fca93901210b81a894359ac
Yugoff <vv.yugoff@gmail.com>
2024-01-23 16:03:42 +0500

    Add Solution 27

--- leetcode-tasks.py
+++ leetcode-tasks.py
@@ -90,4 +90,14 @@ class Solution:
                 unique += 1
                 nums[unique] = nums[i]
         return unique + 1
-        
+
+# 27. Remove Element
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
