9eef22b27e00ddbf4d6867eab164d008f150dc43
Yugoff <vv-yugov@yandex.ru>
2024-01-26 15:57:40 +0500

    Add solution 121

--- leetcode-tasks.py
+++ leetcode-tasks.py
@@ -157,9 +157,21 @@ class Solution:
         nums1 = [num for num in nums1 if num >= 0]
         return nums1
 
-# 108. Convert Sorted Array to Binary Search Tree
+# 121. Best Time to Buy and Sell Stock
     
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        min_price = float('inf')
 
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            else:
+                profit = price - min_price
+                max_profit = max(max_profit, profit)
+        
+        return max_profit
 
 # 2520. Count the Digits That Divide a Number
 
